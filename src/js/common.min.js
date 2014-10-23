14c0988f1fcedd3b68b5c219ae3ce75171a377f4
Eugene Godun <eugene.godun@gmail.com>
2014-10-23 12:56:31 +0300

    grunt

---
+++ src/js/common.min.js
@@ -0,0 +1,18 @@
+head.ready(function() {
+
+	$(document).on("click", function(){
+		$(".js-toggle").removeClass("is-active");
+		$(".js-toggle-el").removeClass("is-active");
+	});
+
+	$(".js-toggle").on("click", function(event){
+		var el = $(this).attr("data-toggle");
+		$(this).toggleClass("is-active");
+		$("."+el).toggleClass("is-active");
+		event.stopPropagation();
+		return false;
+	});
+	$(".js-nav-main").on("click", function(event){
+		event.stopPropagation();
+	});
+});
