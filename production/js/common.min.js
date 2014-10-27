983acd751e30afdd92eaaad8bb1cf3b1714ade00
Eugene Godun <eugene.godun@gmail.com>
2014-10-27 17:28:29 +0200

    promotion_charity.html

--- production/js/common.min.js
+++ production/js/common.min.js
@@ -3,6 +3,9 @@ head.ready(function() {
 	$(document).on("click", function(){
 		$(".js-toggle").removeClass("is-active");
 		$(".js-toggle-el").removeClass("is-active");
+		$("html").removeClass("has-open-nav");
+		$(".js-toggle-nav").removeClass("is-active");
+		$(".js-nav").removeClass("is-active");
 	});
 
 	$(".js-toggle").on("click", function(event){
@@ -12,8 +15,15 @@ head.ready(function() {
 		event.stopPropagation();
 		return false;
 	});
+	
+	$(".js-toggle-nav").on("click", function(event){
+		$("html").toggleClass("has-open-nav");
+		$(this).toggleClass("is-active");
+		$(".js-nav").toggleClass("is-active");
+		event.stopPropagation();
+		return false;
+	});
 	$(".js-nav-main").on("click", function(event){
 		event.stopPropagation();
 	});
-
 });
